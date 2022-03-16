614a76f7430f2db7f16e29bba1fc10a95441a913
sott0n <fix7211@gmail.com>
2022-03-16 22:21:18 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::env;
 use std::io::{stdin, stdout, Write};
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
@@ -13,8 +15,19 @@ fn main() {
         let cmd = parts.next().unwrap();
         let args = parts;
 
-        let mut child = Command::new(cmd).args(args).spawn().unwrap();
-
-        let _ = child.wait();
+        match cmd {
+            "cd" => {
+                // default is `/` as new directory.
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            _ => {
+                let mut child = Command::new(cmd).args(args).spawn().unwrap();
+                let _ = child.wait();
+            }
+        }
     }
 }
