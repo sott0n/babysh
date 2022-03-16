4f85fd306dda7cb6e96dc11dd1a48648c086262a
sott0n <fix7211@gmail.com>
2022-03-16 22:04:31 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
 use std::process::Command;
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
 
-    let cmd = input.trim();
-    Command::new(cmd).spawn().unwrap();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let cmd = parts.next().unwrap();
+        let args = parts;
+
+        let mut child = Command::new(cmd).args(args).spawn().unwrap();
+
+        let _ = child.wait();
+    }
 }
