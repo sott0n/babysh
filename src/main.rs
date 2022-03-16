864d968d9f9f3eac0170d66372ab64dcec37b586
sott0n <fix7211@gmail.com>
2022-03-16 23:35:10 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,50 @@ use std::env;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
 use std::process::Command;
+use std::str::SplitWhitespace;
+
+pub enum Cmd<'a> {
+    Cd(&'a Path),
+    Exit,
+    Other(String, SplitWhitespace<'a>),
+}
+
+pub fn parse<'a>(input: &'a str) -> Cmd<'a> {
+    let mut parts = input.trim().split_whitespace();
+    let cmd = parts.next().unwrap();
+    let args = parts;
+
+    let cmd = match cmd {
+        "cd" => {
+            // default is `/` as new directory.
+            let new_dir = args.peekable().peek().map_or("/", |x| *x);
+            let root = Path::new(new_dir);
+            Cmd::Cd(root)
+        }
+        "exit" => Cmd::Exit,
+        _ => Cmd::Other(cmd.to_owned(), args),
+    };
+    cmd
+}
+
+pub fn exec(cmd: Cmd) {
+    match cmd {
+        Cmd::Cd(root) => {
+            if let Err(e) = env::set_current_dir(&root) {
+                eprintln!("{}", e);
+            }
+        }
+        Cmd::Exit => return,
+        Cmd::Other(cmd, args) => {
+            match Command::new(&cmd).args(args).spawn() {
+                Ok(mut child) => {
+                    let _ = child.wait();
+                }
+                Err(_) => eprintln!("babysh: command not found: {}", cmd),
+            };
+        }
+    }
+}
 
 fn main() {
     loop {
@@ -11,28 +55,7 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let cmd = parts.next().unwrap();
-        let args = parts;
-
-        match cmd {
-            "cd" => {
-                // default is `/` as new directory.
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
-                }
-            }
-            "exit" => return,
-            _ => {
-                match Command::new(cmd).args(args).spawn() {
-                    Ok(mut child) => {
-                        let _ = child.wait();
-                    }
-                    Err(_) => eprintln!("babysh: command not found: {}", cmd),
-                };
-            }
-        }
+        let cmd = parse(&input);
+        exec(cmd);
     }
 }
