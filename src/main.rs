6546826e9afd56e7df164c69f4b37b63a7ea66d3
sott0n <fix7211@gmail.com>
2022-03-17 02:11:32 +0900

    Add pipe

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use std::process::Command;
+use std::process::{exit, Child, Command, Stdio};
 use std::str::SplitWhitespace;
 
 pub enum Cmd<'a> {
@@ -25,25 +25,65 @@ pub fn parse<'a>(input: &'a str) -> Cmd<'a> {
         "exit" => Cmd::Exit,
         _ => Cmd::Other(cmd.to_owned(), args),
     };
+
     cmd
 }
 
-pub fn exec(cmd: Cmd) {
-    match cmd {
+pub fn exec(cmd: Cmd, prev_cmd: Option<Child>, is_next_cmd: bool) -> Option<Child> {
+    let out: Option<Child> = match cmd {
         Cmd::Cd(root) => {
             if let Err(e) = env::set_current_dir(&root) {
                 eprintln!("{}", e);
             }
+            None
         }
-        Cmd::Exit => return,
+        Cmd::Exit => exit(0),
         Cmd::Other(cmd, args) => {
-            match Command::new(&cmd).args(args).spawn() {
-                Ok(mut child) => {
-                    let _ = child.wait();
-                }
-                Err(_) => eprintln!("babysh: command not found: {}", cmd),
+            let stdin = prev_cmd.map_or(Stdio::inherit(), |output: Child| {
+                Stdio::from(output.stdout.unwrap())
+            });
+            let stdout = if is_next_cmd {
+                Stdio::piped()
+            } else {
+                Stdio::inherit()
             };
+
+            let output = Command::new(&cmd)
+                .args(args)
+                .stdin(stdin)
+                .stdout(stdout)
+                .spawn();
+
+            match output {
+                Ok(output) => Some(output),
+                Err(_) => {
+                    eprintln!("babysh: command not found: {}", cmd);
+                    None
+                }
+            }
         }
+    };
+
+    out
+}
+
+pub fn pipe<'a>(inputs: &'a str) {
+    let mut input_list = inputs.trim().split(" | ").peekable();
+    let mut prev_cmd = None;
+
+    while let Some(input) = input_list.next() {
+        let cmd = parse(&input);
+        let is_next_cmd = if input_list.peek().is_some() {
+            true
+        } else {
+            false
+        };
+        let output = exec(cmd, prev_cmd, is_next_cmd);
+        prev_cmd = output;
+    }
+
+    if let Some(mut fin_cmd) = prev_cmd {
+        let _ = fin_cmd.wait();
     }
 }
 
@@ -54,8 +94,6 @@ fn main() {
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
-
-        let cmd = parse(&input);
-        exec(cmd);
+        pipe(&input);
     }
 }
